b81910460f60f93415af29dc67de1508ed0c6d15
fengxiaocan <fengxiaocan@gmail.com>
2017-09-05 15:29:40 +0800

    文件编码转换,添加注释

--- src/com/evil/file/FileTranscoder.java
+++ src/com/evil/file/FileTranscoder.java
@@ -81,6 +81,7 @@ public class FileTranscoder {
 	private JTextField textField_1;
 	private JLabel label_2;
 	private JTextArea textArea;
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -93,9 +94,9 @@ public class FileTranscoder {
 		btnStart.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(selectPath == null || selectPath.equals("")){
+				if (selectPath == null || selectPath.equals("")) {
 					println("请选择转码文件路径");
-				}else{
+				} else {
 					startTrans();
 				}
 			}
@@ -124,89 +125,70 @@ public class FileTranscoder {
 				result = fileChooser.showOpenDialog(frame);
 				if (JFileChooser.APPROVE_OPTION == result) {
 					selectPath = fileChooser.getSelectedFile().getPath();
-					println("已选择转码文件路径:"+selectPath);
+					println("已选择转码文件路径:" + selectPath);
 				}
 			}
 		});
 
 		comboBox1 = new JComboBox();
 		addCharsetItem(comboBox1);
-
+		// 下拉选择器
 		comboBox2 = new JComboBox();
 		addCharsetItem(comboBox2);
 		comboBox2.setSelectedIndex(1);
-		
+
 		label = new JLabel("选择原编码");
 
 		label_1 = new JLabel("选择转编码");
-		
+
 		textField_1 = new JTextField();
-		
+
 		label_2 = new JLabel("输入过滤后缀名");
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(label, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
-					.addGap(14)
-					.addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE)
-					.addGap(53)
-					.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
-					.addGap(6)
-					.addComponent(comboBox2, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(106, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(298)
-					.addComponent(btnStart)
-					.addContainerGap(363, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(219)
-					.addComponent(label_2)
-					.addGap(18)
-					.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 185, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(236, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(8)
-							.addComponent(btnSelect)
-							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 505, GroupLayout.PREFERRED_SIZE))
-						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 662, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(70, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(13)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addContainerGap()
+						.addComponent(label, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE).addGap(14)
+						.addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE).addGap(53)
+						.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE).addGap(6)
+						.addComponent(comboBox2, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(106, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup().addGap(298).addComponent(btnStart).addContainerGap(363,
+						Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup().addGap(219).addComponent(label_2).addGap(18)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 185, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(236, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup().addGroup(groupLayout
+						.createParallelGroup(Alignment.TRAILING, false)
+						.addGroup(groupLayout.createSequentialGroup().addGap(8).addComponent(btnSelect)
+								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(textField, GroupLayout.PREFERRED_SIZE, 505, GroupLayout.PREFERRED_SIZE))
+						.addGroup(Alignment.LEADING,
+								groupLayout.createSequentialGroup().addContainerGap().addComponent(scrollPane,
+										GroupLayout.PREFERRED_SIZE, 662, GroupLayout.PREFERRED_SIZE)))
+						.addContainerGap(70, Short.MAX_VALUE)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
+				.createSequentialGroup().addGap(13)
+				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addComponent(btnSelect, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
 						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))
-					.addGap(10)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(10)
+				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-							.addComponent(comboBox2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addComponent(comboBox1, GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(1)
-							.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
+								.addComponent(comboBox2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(comboBox1, GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE))
+						.addGroup(groupLayout.createSequentialGroup().addGap(1).addComponent(label_1,
+								GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
 						.addComponent(label, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 28, Short.MAX_VALUE)
-						.addComponent(label_2))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnStart)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 487, GroupLayout.PREFERRED_SIZE)
-					.addGap(12))
-		);
-		
+				.addGap(18)
+				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 28, Short.MAX_VALUE).addComponent(label_2))
+				.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnStart)
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 487, GroupLayout.PREFERRED_SIZE).addGap(12)));
+
 		textArea = new JTextArea();
 		textArea.setMargin(new Insets(10, 10, 10, 10));
 		textArea.setFont(new Font("Monospaced", Font.PLAIN, 18));
@@ -216,128 +198,161 @@ public class FileTranscoder {
 		frame.getContentPane().setLayout(groupLayout);
 	}
 
-	public void startTrans(){
+	public void startTrans() {
 		textArea.setText("");
 		final File transFile = new File(selectPath);
 		if (transFile == null) {
 			println("选择转码文件错误!");
-		}else{
-			if(transFile.isFile()){
+		} else {
+			if (transFile.isFile()) {
 				println("开始转码!");
 				newFileToUtf(transFile);
 				println("转码完成!");
-			}else{
+			} else {
 				new Thread(new Runnable() {
 					public void run() {
 						println("开始转码!");
 						String dir = transFile.getAbsolutePath();
-						String parent = dir+"copy";
+						String parent = dir + "copy";
 						File parentDir = new File(parent);
-						findFile(transFile,parentDir);
+						findFile(transFile, parentDir);
 						println("转码完成!");
 					}
 				}).start();
-			}	
+			}
 		}
 	}
-	
-	
+
+	/**
+	 * 给下拉选择框设置选择文本
+	 * 
+	 * @param jcb
+	 *            选择器
+	 */
 	private void addCharsetItem(JComboBox jcb) {
 		for (String string : sCharsetList) {
 			jcb.addItem(string);
 		}
 	}
-	
-	public void print(String str){
+
+	/**
+	 * 打印
+	 * 
+	 * @param str
+	 *            打印文本
+	 */
+	public void print(String str) {
 		textArea.append(str);
 	}
-	
-	public void println(String str){
+
+	public void println(String str) {
 		textArea.append(str);
 		textArea.append("\r\n");
 	}
-	
-	public void findFile(File file,File filePar) {
-        if (file == null) {
-            return;
-        }
-        File[] files1 = file.listFiles(new java.io.FileFilter() {
+
+	/**
+	 * 查找文件
+	 * 
+	 * @param file
+	 *            文件
+	 * @param filePar
+	 *            父文件
+	 */
+	public void findFile(File file, File filePar) {
+		if (file == null) {
+			return;
+		}
+		File[] files1 = file.listFiles(new java.io.FileFilter() {
 			@Override
 			public boolean accept(File pathname) {
-				if(pathname.isDirectory()){
+				if (pathname.isDirectory()) {
 					return true;
 				}
 				String wtfName = textField_1.getText();
-				if(wtfName == null || wtfName.equals("")){
+				if (wtfName == null || wtfName.equals("")) {
 					return true;
 				}
-				if(pathname.getName().endsWith(wtfName)){
+				if (pathname.getName().endsWith(wtfName)) {
 					return true;
 				}
 				return false;
 			}
 		});
-        if (files1 == null) {
-            return;
-        }
-        for (File file1 : files1) {
-            if (file1.isDirectory()) {
-                File dir = new File(filePar,file1.getName());
-                dir.mkdirs();
-                findFile(file1, dir);
-            } else {
-                newFileToUtf(file1,filePar);
-            }
-        }
-    }
+		if (files1 == null) {
+			return;
+		}
+		for (File file1 : files1) {
+			if (file1.isDirectory()) {
+				File dir = new File(filePar, file1.getName());
+				dir.mkdirs();
+				findFile(file1, dir);
+			} else {
+				newFileToUtf(file1, filePar);
+			}
+		}
+	}
 
-	public void newFileToUtf(File file){
-        try{
-        	println("正在转码:"+file.getAbsolutePath());
-            FileInputStream stream = new FileInputStream(file);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stream,(String)comboBox1.getSelectedItem()));
-            String name  = file.getName();
-            int pos = name.lastIndexOf('.');
-            String newName = name.substring(0, pos) +"_copy" +name.substring(pos, name.length());
-            FileOutputStream os = new FileOutputStream(new File(file.getParent(),newName));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,(String)comboBox2.getSelectedItem()));
+	/**
+	 * 把文件转换为新的编码 执行单个文件的转换
+	 * 
+	 * @param file
+	 */
+	public void newFileToUtf(File file) {
+		try {
+			println("正在转码:" + file.getAbsolutePath());
+			FileInputStream stream = new FileInputStream(file);
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(stream, (String) comboBox1.getSelectedItem()));
+			String name = file.getName();
+			int pos = name.lastIndexOf('.');
+			String newName = name.substring(0, pos) + "_copy" + name.substring(pos, name.length());
+			FileOutputStream os = new FileOutputStream(new File(file.getParent(), newName));
+			BufferedWriter writer = new BufferedWriter(
+					new OutputStreamWriter(os, (String) comboBox2.getSelectedItem()));
 
-            char[] arr = new char[1024*8];
-            int len;
-            while((len = reader.read(arr)) > 0){
-                writer.write(arr,0,len);
-                writer.flush();
-            }
-            reader.close();
-            writer.close();
-        }catch(Exception e){
-            println("转码异常:"+e.getMessage());
-            println("转码异常文件:"+file.getAbsolutePath());
-        }
-    }
-	
-    public void newFileToUtf(File file,File filePar){
-        if(!filePar.exists()){
-            filePar.mkdirs();
-        }
-        try{
-        	println("正在转码:"+file.getAbsolutePath());
-            FileInputStream stream = new FileInputStream(file);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stream,(String)comboBox1.getSelectedItem()));
-            FileOutputStream os = new FileOutputStream(new File(filePar,file.getName()));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,(String)comboBox2.getSelectedItem()));
+			char[] arr = new char[1024 * 8];
+			int len;
+			while ((len = reader.read(arr)) > 0) {
+				writer.write(arr, 0, len);
+				writer.flush();
+			}
+			reader.close();
+			writer.close();
+		} catch (Exception e) {
+			println("转码异常:" + e.getMessage());
+			println("转码异常文件:" + file.getAbsolutePath());
+		}
+	}
 
-            char[] arr = new char[1024*8];
-            int len;
-            while((len = reader.read(arr)) > 0){
-                writer.write(arr,0,len);
-                writer.flush();
-            }
-            reader.close();
-            writer.close();
-        }catch(Exception e){
-        	println("转码异常:"+e.getMessage());
-            println("转码异常文件:"+file.getAbsolutePath());
-        }
-    }
+	/**
+	 * 把文件转换为新的编码 执行文件夹下多个文件的转换
+	 * 
+	 * @param file
+	 */
+	public void newFileToUtf(File file, File filePar) {
+		if (!filePar.exists()) {
+			filePar.mkdirs();
+		}
+		try {
+			println("正在转码:" + file.getAbsolutePath());
+			FileInputStream stream = new FileInputStream(file);
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(stream, (String) comboBox1.getSelectedItem()));
+			FileOutputStream os = new FileOutputStream(new File(filePar, file.getName()));
+			BufferedWriter writer = new BufferedWriter(
+					new OutputStreamWriter(os, (String) comboBox2.getSelectedItem()));
+
+			char[] arr = new char[1024 * 8];
+			int len;
+			while ((len = reader.read(arr)) > 0) {
+				writer.write(arr, 0, len);
+				writer.flush();
+			}
+			reader.close();
+			writer.close();
+		} catch (Exception e) {
+			println("转码异常:" + e.getMessage());
+			println("转码异常文件:" + file.getAbsolutePath());
+		}
+	}
 }
